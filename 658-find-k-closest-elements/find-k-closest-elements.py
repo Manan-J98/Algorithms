b0f316b92fcea7fd8c2ea5e28badd8f45d5d4f3c
Manan Juneja <32263521+Manan-J98@users.noreply.github.com>
2024-11-05 17:07:07 -0500

    Time: 75 ms (11.37%) | Memory: 19.2 MB (6.12%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -0,0 +1,14 @@
+class Solution:
+    # main intuition - using a heap
+    # create a max heap and pop when size of heap > k
+    # store value and difference as tuple, sort on the basis of diff from x
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        heap = []
+        heapify(heap)
+        for val in arr:
+            heappush(heap, (-(abs(val-x)), -val))
+            if len(heap) > k:
+                temp = heappop(heap)
+        res = [-t[1] for t in heap]
+        res.sort()
+        return res
