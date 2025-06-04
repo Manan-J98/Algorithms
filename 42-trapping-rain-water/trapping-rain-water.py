11d9a78c62f782d2e88b965d4f34c6bbc71d81ec
Manan Juneja <32263521+Manan-J98@users.noreply.github.com>
2025-06-04 14:23:31 -0400

    Time: 3 ms (97.82%) | Memory: 19.3 MB (69.49%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,18 +1,38 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        maxLeft, maxRight = [], [0] * len(height)
-        curMax = 0
+        # maxLeft, maxRight = [], [0] * len(height)
+        # curMax = 0
+        # res = 0
+        # for h in height:
+        #     maxLeft.append(curMax)
+        #     curMax = max(curMax, h)
+        # curMax = 0
+        # for i in range(len(height)-1, -1, -1):
+        #     maxRight[i] = curMax
+        #     curMax = max(curMax, height[i])
+        # for i in range(len(height)):
+        #     trap = min(maxLeft[i], maxRight[i]) - height[i]
+        #     if trap > 0:
+        #         res += trap
+        # return res
+
+        # TWO POINTER APPROACH
+        left, right = 0, len(height)-1
+        leftMax = rightMax = 0
         res = 0
-        for h in height:
-            maxLeft.append(curMax)
-            curMax = max(curMax, h)
-        curMax = 0
-        for i in range(len(height)-1, -1, -1):
-            maxRight[i] = curMax
-            curMax = max(curMax, height[i])
-        for i in range(len(height)):
-            trap = min(maxLeft[i], maxRight[i]) - height[i]
-            if trap > 0:
-                res += trap
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= leftMax:
+                    leftMax = height[left]
+                else:
+                    res += leftMax - height[left]
+                left += 1
+            else:
+                if height[right] >= rightMax:
+                    rightMax = height[right]
+                else:
+                    res += rightMax - height[right]
+                right -= 1
         return res
+
 
