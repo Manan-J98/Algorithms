d5ccf379f228c954320d2fa693205fb50d835795
Manan Juneja <32263521+Manan-J98@users.noreply.github.com>
2025-06-14 12:16:31 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (13.07%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        cache = [-1] * len(nums)
-        def solve(index):
+        memo = {}
+        def robbing(index):
             if index >= len(nums):
                 return 0
-            if not cache[index] == -1:
-                return cache[index]
-            choice1 = nums[index] + solve(index+2)
-            choice2 = solve(index+1)
-            cache[index] = max(choice1,choice2)
-            return cache[index]
-        return solve(0)
+            if index in memo:
+                return memo[index]
+
+            option1 = robbing(index+1)
+            option2 = nums[index] + robbing(index+2)
+
+            memo[index] = max(option1, option2)
+            return memo[index]
+        return robbing(0)
+        
